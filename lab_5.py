0d4f0058be7679cccef408dacbe3e882faed0b55
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-12-03 19:04:35 -0800

    finish lab

--- lab_5.py
+++ lab_5.py
@@ -40,3 +40,20 @@ create_dictionary(infile):
 main()
 """
 
+def main():
+    sentence = input("Enter a sentence")
+    dictionary = create_dictionary("textese.txt")
+    translate(sentence, dictionary)
+
+def create_dictionary(txt_file):
+    infile = open(txt_file, "r")
+    words = [word.rstrip() for word in infile]
+    infile.close()
+    return dict([word.split(",") for word in words])
+
+def translate(sentence, dictionary):
+    words = sentence.split()
+    for word in words:
+        print(dictionary.get(word, word), " ", end="")
+
+main()
