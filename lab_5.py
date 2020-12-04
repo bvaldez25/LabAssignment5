89ff93200658250e32b528022ed7d5a364ffa41a
bvaldez25 <bvaldez25@mail.fresnostate.edu>
2020-12-03 19:02:03 -0800

    finsh pseudocode

--- lab_5.py
+++ lab_5.py
@@ -19,3 +19,24 @@ After each word is translated, we then
 print out the translated sentence to the user.
 """
 
+"""
+main():
+    set sentence = input()
+    set dictionary = create_dictionary()
+    translate(sentence, dictionary)
+
+translate(sentence, dictionary):
+    words = for each of the word in the sentence
+    for each words, translate the word
+    print translated sentence to user
+
+create_dictionary(infile):
+    read in textese.txt
+    create list = each line from file
+    close the file
+    create a dict off of the list
+    return the dict
+
+main()
+"""
+
